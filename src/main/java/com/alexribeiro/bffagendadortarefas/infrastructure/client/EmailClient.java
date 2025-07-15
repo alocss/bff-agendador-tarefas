8964fd2ac63bcdf02886fde7b03258b5af93ad0d
AlexRibeiro <alexmtt.ribeiro@gmail.com>
2025-07-15 11:19:15 -0300

    Adicionado client de email

---
+++ src/main/java/com/alexribeiro/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,15 @@
+package com.alexribeiro.bffagendadortarefas.infrastructure.client;
+
+
+import com.alexribeiro.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+
+public interface EmailClient {
+
+    void enviarEmail (@RequestBody TarefasDTOResponse dto);
+
+
+}
